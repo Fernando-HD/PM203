06d1d0f44e986d4dae6f8562543a85d89f698639
Fernando-HD <123045484@upq.edu.mx>
2026-06-12 22:42:28 -0600

    Práctica8_Avance 1

--- Practica5/myapp2/App.js
+++ Practica5/myapp2/App.js
@@ -1,34 +1,35 @@
-/*Zona 1: importaciones de componentes y archivos*/
+/* Zona 1: Importanciones de componentes y archivos */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 import { Saludo } from './components/Saludo';
 import { Saludo2 } from './components/Saludo2';
 import { Perfil } from './components/Perfil';
 
-/*Zona 2: zona de los componentes visuales (main)*/
+/* Zona 2: Main - Hogar de los componentes */
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <Text>---Componentes Nativos---</Text>
-      <Image source={require('./assets/wave.png')} />
-      <Text>Hola mundo React Native</Text>
+      <Perfil 
+      nombre="Fernando HD" 
+      carrera="Ing.Sistemas"
+      materia="Programacion movil" 
+      cuatri="9">
+      </Perfil>
 
-      <Text>---Componente Propio Simple---</Text>
-      <Saludo></Saludo>
-
-      <Text>---Componente Propio Compuesto---</Text>
-      <Saludo2></Saludo2>
-
-      <Text>---Componente Perfil---</Text>
-      <Perfil></Perfil>
-
+      <Perfil 
+      nombre="FER HD" 
+      carrera="Ing. Sistemas"
+      materia="Programacion movil" 
+      cuatri="9" />
+      
       <StatusBar style="auto" />
+
     </View>
   );
 }
 
-/*Zona 3: estilos y posicionamiento*/
+/* Zona 3: Estilos y Posicionamiento  */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- Practica5/myapp2/components/Perfil.js
+++ Practica5/myapp2/components/Perfil.js
@@ -1,12 +1,48 @@
-import { View, Text } from 'react-native';
+/* Perfil usando Desestructuracion */
+import { Text, View, Button, StyleSheet } from "react-native";
+import React, {useState} from "react";
 
-export const Perfil = () => {
-    return (
-        <View>
-            <Text>Nombre: Luis Fernando Hernández Dimas</Text>
-            <Text>Carrera: Ingeniería en Sistemas Computacionales</Text>
-            <Text>Materia: Programación Móvil</Text>
-            <Text>Cuatrimestre: 10°</Text>
+export const Perfil=({nombre, carrera, materia, cuatri})=>{
+    const [mostrar, setMostrar] = useState(false);
+
+    return(
+        <View style={styles.tarjeta}>
+            <Text style={styles.nombre}>{nombre}</Text>
+
+            {/* Renderizado condicional */}
+            {mostrar && 
+            <>
+            <Text style={styles.carrera}>{carrera}</Text>
+            <Text style={styles.otroTexto}>{materia}</Text>
+            <Text style={styles.otroTexto}>{cuatri}</Text>
+            </>
+            }
+
+            <Button title="Mostrar Perfil" onPress={() =>setMostrar(!mostrar)}></Button>
+            
         </View>
-    );
+    )
 }
+
+const styles =StyleSheet.create({
+    nombre:{
+        fontSize: 24,
+        fontWeight: 600,
+        textTransform: 'uppercase'
+    },
+    carrera:{
+        fontSize: 18,
+        color: 'blue',
+        fontFamily: 'Roboto',
+    },
+    otroTexto:{
+        fontSize: 12,
+        fontFamily: 'Courier',
+        fontStyle: 'italic',
+    },
+    tarjeta:{
+        borderWidth: 2,
+        padding: 25,
+        margin: 15,
+    },
+})
